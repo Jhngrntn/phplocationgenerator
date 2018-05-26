f85399261c223840c84a568acc3f0015a784fb94
Jhngrntn <Johngrntn@gmail.com>
2018-05-26 18:14:01 -0400

    Add files via upload

---
+++ locations.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+	</head>
+	
+	
+	<body>
+		<?php
+			$locale = array("city", "town", "harbor", "trailor park", "village", "tavern", "orphanage", "college campus");
+			
+			$country = array("United States", "France", "England", "Canada", "New Zealand", "Russia", "Norway", "Ukraine");
+			
+			$climate = array("deep the dead of winter", "amidst the heat of summer", "during the fall", "during the spring");
+			
+			$rand1 = array_rand($locale, 1);
+			$rand2 = array_rand($country, 1);
+			$rand3 = array_rand($climate, 1);
+			
+			echo "A ".$locale[$rand1]." in ".$country[$rand2]." ".$climate[$rand3].".";
+		?>
+
+	</body>
+</html>
